9ff9b419449739533c264df1e437362dc37051e0
Pupett <pupettaj@gmail.com>
2024-06-09 14:10:24 -0400

    Updated pool

--- models/database.js
+++ models/database.js
@@ -1,12 +1,20 @@
 const pg = require('pg');
 const { Pool } = pg;
 
-const pool = new Pool({
-  host: process.env.DB_HOST || 'localhost',
-  port: process.env.DB_PORT || 5432,
-  database: process.env.DB_DATABASE,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-});
+let pool = null;
+
+if (process.env.DB_URL) {
+  pool = new Pool({
+    connectionString: process.env.DB_URL,
+  });
+} else {
+  pool = new Pool({
+    host: process.env.DB_HOST || 'localhost',
+    port: process.env.DB_PORT || 5432,
+    database: process.env.DB_DATABASE,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+  });
+}
 
 module.exports = pool;
